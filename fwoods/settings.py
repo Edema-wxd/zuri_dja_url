6d46a9a9e88499828eaac0a80f150942daa42569
Edema-wxd <100766884+Edema-wxd@users.noreply.github.com>
2022-06-19 16:29:44 +0100

    changed the config

--- fwoods/settings.py
+++ fwoods/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-igq7&_c3o$*(0p#17^5a@sju5lywt@pvq8=)e+k(4_e6m3oc5p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
